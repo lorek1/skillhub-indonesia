0134fdb2affbe1f0deab9d9c4b694c44f871201b
Данила <danila@MacBook-Pro-Danila.local>
2025-10-28 14:42:56 +0300

    Add robots.txt and sitemap.xml - allow Facebook bot access

---
+++ src/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://skillhubindonesia.site/sitemap.xml',
+  }
+}
