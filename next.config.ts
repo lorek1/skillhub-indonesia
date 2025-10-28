ef5a0cbd936343a70d77fe4ee14ea7853495596c
Данила <danila@MacBook-Pro-Danila.local>
2025-10-28 15:07:48 +0300

    Simplify config: remove middleware, use next.config headers only

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow, all',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
