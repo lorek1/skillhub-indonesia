73e94ee566e42fafd5f9c610a7b9ab55cb8c2cc6
Данила <danila@MacBook-Pro-Danila.local>
2025-10-28 15:22:58 +0300

    Remove all blocking elements: robots.txt, sitemap, custom headers, verification tags

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'index, follow, all',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
